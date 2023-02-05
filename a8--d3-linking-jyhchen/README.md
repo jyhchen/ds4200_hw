1a13189bad2bbbdb860ad9a5816ae12ff12ebbc8
Justin Chen <chen.justi@northeastern.edu>
2023-02-04 19:41:22 -0500

    Add files via upload

---
+++ a8--d3-linking-jyhchen/README.md
@@ -0,0 +1,5 @@
+# A8—D3 linking
+
+This repository is your starting point for the assignment. See the assignment instructions on the course website.
+
+Link to your GitHub pages website: <insert your clickable hyperlink here>
---
+++ a8--d3-linking-jyhchen/js/visualization.js
@@ -0,0 +1,38 @@
+// Immediately Invoked Function Expression to limit access to our 
+// variables and prevent 
+((() => {
+
+    // Load the data from a json file (you can make these using
+    // JSON.stringify(YOUR_OBJECT), just remove the surrounding '')
+    d3.json('data/texas.json').then(data => {
+
+        // Create a line chart given x and y attributes, labels, offsets;
+        // a div id selector to put our svg in; and the data to use.
+        let lcYearPoverty = linechart()
+            .x(d => d.year)
+            .xLabel('YEAR')
+            .y(d => d.poverty)
+            .yLabel('POVERTY RATE')
+            .yLabelOffset(40)
+            ('#linechart', data);
+
+
+
+        // Create a scatterplot given x and y attributes, labels, offsets;
+        // a div id selector to put our svg in; and the data to use.
+        let spUnemployMurder = scatterplot()
+            .x(d => d.unemployment)
+            .xLabel('UNEMPLOYMENT RATE')
+            .y(d => d.murder)
+            .yLabel('MURDER RATE IN STATE PER 100000')
+            .yLabelOffset(150)
+            ('#scatterplot', data);
+
+        let dispatch = d3.dispatch('line_to_scatter', 'scatter_to_line');
+
+        lcYearPoverty.selectionDispatcher(dispatch.on('line_to_scatter', spUnemployMurder.updateSelection));
+        spUnemployMurder.selectionDispatcher(dispatch.on('scatter_to_line', lcYearPoverty.updateSelection));
+
+    });
+
+})());
